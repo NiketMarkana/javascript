360a122e36d9538b7b97310d9a683c1d1d0509e8
NiketMarkana <159057847+NiketMarkana@users.noreply.github.com>
2025-08-17 10:20:20 +0000

    arrays part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -8,7 +8,7 @@ const dc_heros = ["superman", "flash", "batman"]
 
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
-
+ 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
